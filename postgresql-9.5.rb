e2e3810acea14e21eefa2b3f82b425ee42d922e6
Oleksii Kliukin <okliukin@zalando-03853.corp.ad.zalando.net>
2014-11-03 15:05:58 +0100

    use version of the patch in the mailing list.

--- postgresql-9.5.rb
+++ postgresql-9.5.rb
@@ -9,7 +9,9 @@ class Postgresql95 < Formula
     depends_on 'petere/sgml/docbook-dsssl' => :build
     depends_on 'petere/sgml/docbook-sgml' => :build
     depends_on 'petere/sgml/openjade' => :build
-    patch :DATA
+    patch do
+    	url "http://www.postgresql.org/message-id/attachment/30899/pgpass_host_wildcard.diff"
+    end
   end
 
   keg_only 'The different provided versions of PostgreSQL conflict with each other.'
@@ -40,7 +42,7 @@ class Postgresql95 < Formula
     args << "--with-extra-version=+git" if build.head?
 
     system "./configure", *args
-    system "make install-world"
+    system "make install"
   end
 
   def caveats; <<-EOS.undent
@@ -58,61 +60,3 @@ class Postgresql95 < Formula
   end
 end
 
-
-__END__
-diff --git a/src/interfaces/libpq/fe-connect.c b/src/interfaces/libpq/fe-connect.c
-index 3fe8c21..7832324 100644
---- a/src/interfaces/libpq/fe-connect.c
-+++ b/src/interfaces/libpq/fe-connect.c
-@@ -373,7 +373,7 @@ static int parseServiceFile(const char *serviceFile,
-         PQconninfoOption *options,
-         PQExpBuffer errorMessage,
-         bool *group_found);
--static char *pwdfMatchesString(char *buf, char *token);
-+static char *pwdfMatchesString(char *buf, char *token, bool is_hostname);
- static char *PasswordFromFile(char *hostname, char *port, char *dbname,
-         char *username);
- static bool getPgPassFilename(char *pgpassfile);
-@@ -5542,7 +5542,7 @@ defaultNoticeProcessor(void *arg, const char *message)
-  * token doesn't match
-  */
- static char *
--pwdfMatchesString(char *buf, char *token)
-+pwdfMatchesString(char *buf, char *token, bool is_hostname)
- {
-  char     *tbuf,
-         *ttok;
-@@ -5556,6 +5556,18 @@ pwdfMatchesString(char *buf, char *token)
-    return tbuf + 2;
-  while (*tbuf != 0)
-  {
-+   /* '*' matches everything until '.' or end, but only for the hostname */
-+   if (*tbuf == '*' && (*(tbuf + 1) == '.' || *(tbuf + 1) == ':') &&
-+     !bslash && is_hostname)
-+   {
-+     tbuf++;
-+     while (*ttok != *tbuf)
-+     {
-+       if (*ttok == 0)
-+         return (*tbuf == ':') ? tbuf + 1 : NULL;
-+       ttok++;
-+     }
-+   }
-    if (*tbuf == '\\' && !bslash)
-    {
-      tbuf++;
-@@ -5664,10 +5676,10 @@ PasswordFromFile(char *hostname, char *port, char *dbname, char *username)
-    if (buf[len - 1] == '\n')
-      buf[len - 1] = 0;
- 
--   if ((t = pwdfMatchesString(t, hostname)) == NULL ||
--     (t = pwdfMatchesString(t, port)) == NULL ||
--     (t = pwdfMatchesString(t, dbname)) == NULL ||
--     (t = pwdfMatchesString(t, username)) == NULL)
-+   if ((t = pwdfMatchesString(t, hostname, true)) == NULL ||
-+     (t = pwdfMatchesString(t, port, false)) == NULL ||
-+     (t = pwdfMatchesString(t, dbname, false)) == NULL ||
-+     (t = pwdfMatchesString(t, username, false)) == NULL)
-      continue;
-    ret = strdup(t);
-    fclose(fp);
