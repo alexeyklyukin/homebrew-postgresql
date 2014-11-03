2cc56ab239ca9371f4e8dff3428cbb2761551f3b
Oleksii Kliukin <okliukin@zalando-03853.corp.ad.zalando.net>
2014-11-03 14:32:28 +0100

    another version of the pgpass patch.

--- postgresql-9.5.rb
+++ postgresql-9.5.rb
@@ -61,85 +61,58 @@ end
 
 __END__
 diff --git a/src/interfaces/libpq/fe-connect.c b/src/interfaces/libpq/fe-connect.c
-new file mode 100644
-index 18fcb0c..003739f
-*** a/src/interfaces/libpq/fe-connect.c
---- b/src/interfaces/libpq/fe-connect.c
-*************** static int parseServiceFile(const char *
-*** 373,379 ****
-           PQconninfoOption *options,
-           PQExpBuffer errorMessage,
-           bool *group_found);
-! static char *pwdfMatchesString(char *buf, char *token);
-  static char *PasswordFromFile(char *hostname, char *port, char *dbname,
-           char *username);
-  static bool getPgPassFilename(char *pgpassfile);
---- 373,379 ----
-           PQconninfoOption *options,
-           PQExpBuffer errorMessage,
-           bool *group_found);
-! static char *pwdfMatchesString(char *buf, char *token, bool is_hostname);
-  static char *PasswordFromFile(char *hostname, char *port, char *dbname,
-           char *username);
-  static bool getPgPassFilename(char *pgpassfile);
-*************** defaultNoticeProcessor(void *arg, const 
-*** 5466,5472 ****
-   * token doesn't match
-   */
-  static char *
-! pwdfMatchesString(char *buf, char *token)
+index 3fe8c21..7832324 100644
+--- a/src/interfaces/libpq/fe-connect.c
++++ b/src/interfaces/libpq/fe-connect.c
+@@ -373,7 +373,7 @@ static int parseServiceFile(const char *serviceFile,
+         PQconninfoOption *options,
+         PQExpBuffer errorMessage,
+         bool *group_found);
+-static char *pwdfMatchesString(char *buf, char *token);
++static char *pwdfMatchesString(char *buf, char *token, bool is_hostname);
+ static char *PasswordFromFile(char *hostname, char *port, char *dbname,
+         char *username);
+ static bool getPgPassFilename(char *pgpassfile);
+@@ -5542,7 +5542,7 @@ defaultNoticeProcessor(void *arg, const char *message)
+  * token doesn't match
+  */
+ static char *
+-pwdfMatchesString(char *buf, char *token)
++pwdfMatchesString(char *buf, char *token, bool is_hostname)
+ {
+  char     *tbuf,
+         *ttok;
+@@ -5556,6 +5556,18 @@ pwdfMatchesString(char *buf, char *token)
+    return tbuf + 2;
+  while (*tbuf != 0)
   {
-    char     *tbuf,
-           *ttok;
---- 5466,5472 ----
-   * token doesn't match
-   */
-  static char *
-! pwdfMatchesString(char *buf, char *token, bool is_hostname)
-  {
-    char     *tbuf,
-           *ttok;
-*************** pwdfMatchesString(char *buf, char *token
-*** 5480,5485 ****
---- 5480,5497 ----
-      return tbuf + 2;
-    while (*tbuf != 0)
-    {
-+     /* '*' matches everything until '.' or end, but only for the hostname */
-+     if (*tbuf == '*' && (*(tbuf + 1) == '.' || *(tbuf + 1) == ':') &&
-+       !bslash && is_hostname)
++   /* '*' matches everything until '.' or end, but only for the hostname */
++   if (*tbuf == '*' && (*(tbuf + 1) == '.' || *(tbuf + 1) == ':') &&
++     !bslash && is_hostname)
++   {
++     tbuf++;
++     while (*ttok != *tbuf)
 +     {
-+       tbuf++;
-+       while (*ttok != *tbuf)
-+       {
-+         if (*ttok == 0)
-+           return (*tbuf == ':') ? tbuf + 1 : NULL;
-+         ttok++;
-+       }
++       if (*ttok == 0)
++         return (*tbuf == ':') ? tbuf + 1 : NULL;
++       ttok++;
 +     }
-      if (*tbuf == '\\' && !bslash)
-      {
-        tbuf++;
-*************** PasswordFromFile(char *hostname, char *p
-*** 5588,5597 ****
-      if (buf[len - 1] == '\n')
-        buf[len - 1] = 0;
-  
-!     if ((t = pwdfMatchesString(t, hostname)) == NULL ||
-!       (t = pwdfMatchesString(t, port)) == NULL ||
-!       (t = pwdfMatchesString(t, dbname)) == NULL ||
-!       (t = pwdfMatchesString(t, username)) == NULL)
-        continue;
-      ret = strdup(t);
-      fclose(fp);
---- 5600,5609 ----
-      if (buf[len - 1] == '\n')
-        buf[len - 1] = 0;
-  
-!     if ((t = pwdfMatchesString(t, hostname, true)) == NULL ||
-!       (t = pwdfMatchesString(t, port, false)) == NULL ||
-!       (t = pwdfMatchesString(t, dbname, false)) == NULL ||
-!       (t = pwdfMatchesString(t, username, false)) == NULL)
-        continue;
-      ret = strdup(t);
-      fclose(fp);
++   }
+    if (*tbuf == '\\' && !bslash)
+    {
+      tbuf++;
+@@ -5664,10 +5676,10 @@ PasswordFromFile(char *hostname, char *port, char *dbname, char *username)
+    if (buf[len - 1] == '\n')
+      buf[len - 1] = 0;
+ 
+-   if ((t = pwdfMatchesString(t, hostname)) == NULL ||
+-     (t = pwdfMatchesString(t, port)) == NULL ||
+-     (t = pwdfMatchesString(t, dbname)) == NULL ||
+-     (t = pwdfMatchesString(t, username)) == NULL)
++   if ((t = pwdfMatchesString(t, hostname, true)) == NULL ||
++     (t = pwdfMatchesString(t, port, false)) == NULL ||
++     (t = pwdfMatchesString(t, dbname, false)) == NULL ||
++     (t = pwdfMatchesString(t, username, false)) == NULL)
+      continue;
+    ret = strdup(t);
+    fclose(fp);
